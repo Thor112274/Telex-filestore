35817cca4915db9cbe4d0df404e62ed16f4e47f4
Thor112274 <126616021+Thor112274@users.noreply.github.com>
2025-01-01 01:40:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
-        self.LOGGER(__name__).info(r"""       
+        self.LOGGER(__name__).info(f"""       
 
 
   ___ ___  ___  ___ ___ _    _____  _____  ___ _____ ___ 
